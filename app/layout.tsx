3c51ba92f0510b30d75d4936677a2e4af69a62a1
rmthomas <rmthomas1998@gmail.com>
2025-02-20 22:02:27 -0500

    update viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import { Nav } from "@/components/layout/nav";
 import { Footer } from "@/components/layout/footer";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
 
 export default function RootLayout({
   children,

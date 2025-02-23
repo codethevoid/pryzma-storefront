9fc0d1ea07abe0c86bd2b33c59bdd6813afaa4d1
rmthomas <rmthomas1998@gmail.com>
2025-02-23 10:19:25 -0500

    update product shell

--- components/layout/product-shell.tsx
+++ components/layout/product-shell.tsx
@@ -5,7 +5,10 @@ import { ProductDetails } from "../ui/product-details";
 export const ProductShell = ({ product }: { product: StoreProduct }) => {
   return (
     <div className="grid grid-cols-5 gap-10 max-[900px]:grid-cols-1 max-[900px]:gap-0 max-[900px]:space-y-6">
-      <section aria-label="Product image gallery" className="sticky top-4 col-span-3 h-fit">
+      <section
+        aria-label="Product image gallery"
+        className="sticky top-4 col-span-3 h-fit max-[900px]:static"
+      >
         <ImageGallery product={product} />
       </section>
       <section aria-label="Product information" className="col-span-2">
